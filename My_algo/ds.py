f21854613ea0d6635cd7fb706309049b0d7e3b70
peropero1 <peropero1@gmail.com>
2021-03-28 22:55:59 +0800

    20210328_from_debug

---
+++ My_algo/ds.py
@@ -0,0 +1,30 @@
+import hyperloglog
+
+class Node():
+    def __init__(self,count=0):
+        self.count=count
+    def add_count(self,count=1):
+        self.count+=count
+    def __str__(self):
+        return 'count: {}'.format(self.count)
+    def __repr__(self):
+        return ''
+
+class Head(Node):
+    def __init__(self,count=0):
+        super().__init__(count)
+        self.distinct = hyperloglog.HyperLogLog(0.01)
+        self.maxID=''
+    def __str__(self):
+        return '[total count: {}, distinct: {}]'.format(self.count,len(self.distinct))
+    def __repr__(self):
+        return '[total count: {}, distinct: {}]'.format(self.count,len(self.distinct))
+
+class Tail(Node):
+    def __init__(self,ID,count):
+        self.ID = ID
+        super().__init__(count)
+    def __str__(self):
+        return '[ID: {}, count: {}]'.format(self.ID,self.count)
+    def __repr__(self):
+        return '[ID: {}, count: {}]'.format(self.ID,self.count)
